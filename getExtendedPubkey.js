d58934809d45da8a1b7926b33b79d253a8b5a674
Zhang Zengbo <zengbo.zhang@gmail.com>
2020-05-12 16:32:55 +0800

    getExtendedPubkey.js

---
+++ getExtendedPubkey.js
@@ -0,0 +1,33 @@
+
+const client = require('./client');
+
+if (process.argv.length != 3 ){
+	console.error("usage : ", process.argv[1], "<path i.e \"bip32/m/44'/0'/0'/0/0\">");
+	process.exit(1);
+}
+
+client.open().then( ()=>{
+    var devId;
+    return client.call("getDeviceList",{})
+}).then( r=> {
+        console.log(r);
+        if( r.errcode == 0 && r.data.length > 0){
+            devId = r.data[0].deviceId;
+            return client.call("connectDevice", devId)
+        }
+        return false;
+}).then( r=>{
+        if( r ) {
+            console.log(r);
+            if( r.errcode == 0 ) {
+                return client.call("getExtendedPubkeyFromPath", process.argv[2]);
+            }
+        }
+        return false;
+}).then( r=>{
+        console.log(r);
+	process.exit(0);
+}).catch( e=>{
+       process.exit(1);
+});
+
